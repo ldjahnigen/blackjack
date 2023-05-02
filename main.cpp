f391686285b1addebd262ce9e84417619ac8bebc
Louis <ldjahnigen@gmail.com>
2023-05-02 08:09:16 -0400

    turn cycle

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@
 #include <sstream>
 #include <string>
 #include <chrono>
+#include <unordered_map>
 
 class Stack {
   public:
     std::vector<std::string> cards;
     int number;
+    int value;
     Stack() {
       cards = {};
       number = 0;
+      value = 0;
     }
 
     void shuffle() {
@@ -31,15 +34,35 @@ class Stack {
       }
     }
 
+    void assessValue() {  
+      std::unordered_map<char, int> values = {
+        {'2',2},
+        {'3',3},
+        {'4',4},
+        {'5',5},
+        {'6',6},
+        {'7',7},
+        {'8',8},
+        {'9',9},
+        {'0',10},
+        {'J',10},
+        {'Q',10},
+        {'K',10},
+        {'A',11}
+      };
+      for (int i = 0; i < cards.size(); i++) {
+        value += values[cards[i][0]];
+      }
+    }
 };
 
 class Deck: public Stack {
   public:
     Deck() {
-      cards = {"1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "Jh", "Qh", "Kh", "Ah",
-              "1d", "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "10d", "Jd", "Qd", "Kd", "Ad",
-              "1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "Js", "Qs", "Ks", "As",
-              "1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "Jh", "Qh", "Kh", "Ah"};
+      cards = {"2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "0h", "Jh", "Qh", "Kh", "Ah",
+               "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "0d", "Jd", "Qd", "Kd", "Ad",
+               "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "0s", "Js", "Qs", "Ks", "As",
+               "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "0h", "Jh", "Qh", "Kh", "Ah"};
       number = cards.size();
     }
 };
@@ -55,15 +78,62 @@ void printCard(std::string card) {
   }
 }
 
-int main() {
-  Stack hand = Stack();
-  Deck deck = Deck();
-
-  deck.shuffle();
-  hand.dealCard(2, deck);
+int cycle(Stack hand, Deck deck, Stack dealer, int playermoney) {
+  std::cout << "YOU HAVE $" << playermoney << '\n';
+  std::cout << "YOUR CARDS ARE: " << '\n';
   for (int i = 0; i < hand.number; i++) {
     printCard(hand.cards[i]);
   }
+  std::cout << "HIT? $10 (y/n): ";
+  char input;
+  std::cin >> input;
+  if (input == 'y' || input == 'Y') {
+    hand.dealCard(1, deck);
+    playermoney -= 10;    
+    hand.assessValue();
+    if (hand.value > 21) {
+      for (int i = 0; i < hand.number; i++) {
+        printCard(hand.cards[i]);
+      }
+      std::cout << "YOU LOSE!\n";
+      return 0;
+    }
+    cycle(hand, deck, dealer, playermoney);
+  } 
+  else if (input == 'n' || input == 'N') {
+    hand.assessValue();
+    dealer.assessValue(); 
+    std::cout << "YOUR HAND:\n";
+    for (int i = 0; i < hand.number; i++) {
+      printCard(hand.cards[i]);
+    }
+    std::cout << "DEALER'S HAND:\n";
+    for (int i = 0; i < dealer.number; i++) {
+      printCard(dealer.cards[i]);
+    }
+    if (hand.value > dealer.value || dealer.value > 21) {
+      std::cout << "YOU WIN!\n";
+    } else if (hand.value < dealer.value || hand.value > 21) {
+      std::cout << "YOU LOSE!\n";
+    } else {
+      std::cout << "TIE\n";
+    }
+  } else {
+    std::cout << "PLEASE ENTER A VALID INPUT" << '\n';
+    cycle(hand, deck, dealer, playermoney);
+  }
+  return 0;
+}
 
+int main() {
+  Stack hand = Stack();
+  Stack dealer = Stack();
+  Deck deck = Deck();
+  int playermoney = 100;
+  deck.shuffle();
+  hand.dealCard(2, deck);
+  dealer.dealCard(2, deck);
+  cycle(hand, deck, dealer, playermoney);
+  hand.assessValue();
   return 0;
 }
